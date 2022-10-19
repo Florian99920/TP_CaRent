165490bacbfb4171bd1d95730b2882d2dd61e8b8
POVOAS Florian <florian_povoas@icloud.com>
2022-10-19 18:01:43 +0200

    PHP fini

--- menu.php
+++ menu.php
@@ -9,7 +9,7 @@ header('Content-Type: text/html; charset=utf-8');
 
 
 function index() : string {
-    $temp = <<<END
+    return <<<END
                 <form id="deco" method="post" action="?action=deco">
                     <button type="submit">Deconnexion</button>
                 </form>
@@ -34,7 +34,7 @@ function index() : string {
 
                 <form id="question3" method="post" action="?action=question3">
                     <input type="text" name="modele" placeholder="modele du vehicule" required><br>
-                    <input type="number" name=jours" placeholder="nombre de jours de location" required><br>
+                    <input type="number" name="jours" placeholder="nombre de jours de location" required><br>
                     <button type="submit">Submit</button><br>
                 </form>
 
@@ -54,14 +54,13 @@ function index() : string {
                     <button type="submit">Submit</button><br>
                 </form>
                END;
-    return $temp;
 }
 
 if (!isset($_SESSION['database'])) {
     header('Location: index.php');
 }
 
-$action = isset($_GET['action']) ? $_GET['action'] : null;
+$action = $_GET['action'] ?? null;
 
 $html = '';
 
@@ -76,20 +75,129 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             header('Location: index.php');
             break;
         case 'question1':
+            $categorie = filter_var($_POST['categorie'], FILTER_SANITIZE_SPECIAL_CHARS);
+            $dateD = filter_var($_POST['dateD'], FILTER_SANITIZE_SPECIAL_CHARS);
+            $dateF = filter_var($_POST['dateF'], FILTER_SANITIZE_SPECIAL_CHARS);
+            $dateD = date('Y/m/d', strtotime($dateD));
+            $dateF = date('Y/m/d', strtotime($dateF));
 
+            $statement = $bdd->prepare(<<<END
+                SELECT DISTINCT VEHICULE.NO_IMM, MODELE FROM VEHICULE where CODE_CATEG = :categorie
+                EXCEPT 
+                SELECT DISTINCT VEHICULE.NO_IMM, MODELE FROM VEHICULE 
+                inner join CALENDRIER on VEHICULE.NO_IMM = CALENDRIER.NO_IMM where CODE_CATEG = :categorie
+                and (DATEJOUR BETWEEN :dateD and :dateF) and PASLIBRE IS NOT NULL
+            END);
+            $statement->bindParam(':categorie', $categorie);
+            $statement->bindParam(':dateD', $dateD);
+            $statement->bindParam(':dateF', $dateF);
+
+            $statement->execute();
+
+            $temp = '';
+
+            while ($row = $statement->fetch()) {
+                $temp .= '<tr>';
+                $temp .= '<td>' . $row['NO_IMM'] . '</td>';
+                $temp .= '<td>' . $row['MODELE'] . '</td>';
+                $temp .= '</tr>';
+            }
+
+            if ($temp == '') {
+                $temp = '<p>Aucun résultat</p>';
+            } else {
+                $temp = <<<END
+                    <table>
+                        <tr>
+                            <th>NO_IMM</th>
+                            <th>MODELE</th>
+                        </tr>
+                        $temp
+                    </table>
+                    END;
+            }
+
+            $html .= $temp;
 
             break;
         case 'question2':
+            $immat = filter_var($_POST['immat'], FILTER_SANITIZE_SPECIAL_CHARS);
+            $dateD = filter_var($_POST['dateD'], FILTER_SANITIZE_SPECIAL_CHARS);
+            $dateF = filter_var($_POST['dateF'], FILTER_SANITIZE_SPECIAL_CHARS);
+            $dateD = date('Y/m/d', strtotime($dateD));
+            $dateF = date('Y/m/d', strtotime($dateF));
+
+            $statement = $bdd->prepare(<<<END
+                SELECT no_imm from calendrier where no_imm = :immat
+                    and NOT EXISTS (SELECT no_imm from calendrier where DATEJOUR 
+                    between :dateD and :dateF and no_imm = :immat and paslibre = 'x')
+            END);
+            $statement->bindParam(':immat', $immat);
+            $statement->bindParam(':dateD', $dateD);
+            $statement->bindParam(':dateF', $dateF);
+
+            $statement->execute();
+
+            if ($row = $statement->fetch()) {
+                $statement = $bdd->prepare(<<<END
+                    UPDATE calendrier set paslibre = 'x' where no_imm = :immat
+                        and DATEJOUR between :dateD and :dateF
+                END);
+                $statement->bindParam(':immat', $immat);
+                $statement->bindParam(':dateD', $dateD);
+                $statement->bindParam(':dateF', $dateF);
+
+                $statement->execute();
+
+                $html .= '<p>La mise à jour a été effectuée</p>';
+            } else {
+                $html .= '<p>Le vehicule n\'est pas disponible pour cette periode</p>';
+            }
+
             break;
         case 'question3':
+            $modele = filter_var($_POST['modele'], FILTER_SANITIZE_SPECIAL_CHARS);
+            $jours = (int) filter_var($_POST['jours'], FILTER_SANITIZE_NUMBER_INT);
+
+            $statement = $bdd->prepare(<<<END
+                select tarif_hebdo*(:jours-MOD(:jours,7))/7 + tarif_jour* MOD(:jours,7) from vehicule
+                    inner join categorie on vehicule.code_categ = categorie.code_categ
+                    inner join tarif on categorie.code_tarif = tarif.code_tarif
+                    where modele = :modele
+            END);
+            $statement->bindParam(':jours', $jours);
+            $statement->bindParam(':modele', $modele);
+
+            $statement->execute();
+
+            $temp = '';
+
+            if ($row = $statement->fetch()) {
+                $temp = '<p>Le montant de la location est de ' . $row[0] . '€</p>';
+            } else {
+                $temp = '<p>Aucun résultat</p>';
+            }
+
+            $html .= $temp;
+
             break;
         case 'question4':
             $statement = $bdd->prepare("SELECT CODE_AG FROM AGENCE where CODE_AG not in (select CODE_AG from AGENCE where CODE_AG not in(select DISTINCT CODE_AG from VEHICULE))");
             $statement->execute();
 
+            $temp = '';
+
             while ($result = $statement->fetch()) {
-                $html .= "<p>{$result[0]}</p>";
+                $temp .= "<p>$result[0]</p>";
             }
+
+            if ($temp == '') {
+                $temp = '<p>Aucun resultat</p>';
+            } else {
+                $temp = '<p>Les agences qui possèdent toutes les catégories de véhicules sont :</p>' . $temp;
+            }
+
+            $html .= $temp;
 
             break;
         case 'question5':
@@ -97,9 +205,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $statement = $bdd->prepare("select NOM,VILLE,CODPOSTAL from CLIENT where CODE_CLI in(select CODE_CLI from DOSSIER D1 where D1.CODE_CLI in (select CODE_CLI from DOSSIER where NO_IMM != D1.NO_IMM))");
             $statement->execute();
 
+            $temp = '';
+
             while ($result = $statement->fetch()) {
-                $html .= "<p>{$result[0]} {$result[1]} {$result[2]}</p>";
+                $temp .= "<p>$result[0] $result[1] $result[2]</p>";
             }
+
+            if ($temp == '') {
+                $temp = '<p>Aucun resultat</p>';
+            } else {
+                $temp = '<p>Les clients qui ont loué deux modèles différents de voiture sont :</p>' . $temp;
+            }
+
+            $html .= $temp;
 
             break;
         default:
